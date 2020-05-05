94e940ecd43323969af75586da5d46140332c72b
Jonathon Mitterbacher <s3784464@student.rmit.edu.au>
2020-05-05 17:31:22 +1000

    Report document created

--- weather.py
+++ weather.py
@@ -7,11 +7,11 @@ api = Api(api_key)
 
 # Set the granularity of the API - Options: ['daily','hourly','3hourly']
 # Will only affect forecast requests.
-api.set_granularity('hourly')
+api.set_granularity('daily')
 
 forecast = api.get_forecast(city="Geelong", state="Victoria", country="AU", units='M')
-#print(forecast.get_series('temp'))
-print(forecast.get_history())
+print(forecast.get_series(['temp','precip']))
+#print(api.get_history(city="Geelong", state="Victoria", country="AU", units='M', start_date="2020-04-04", end_date="2020-04-05"))
 
 
 # # You can also query by city:
@@ -19,9 +19,6 @@ print(forecast.get_history())
 
 # # Or City, state, and country:
 # forecast = api.get_forecast(city="Raleigh", state="North Carolina", country="US")
-
-# # To get a daily forecast of temperature, and precipitation:
-# print forecast.get_series(['temp','precipitation'])
 
 
 # # To get a daily time series of temperature, precipitation, and rh:
@@ -31,10 +28,5 @@ print(forecast.get_history())
 # api.set_granularity('hourly')
 # history = api.get_history(lat=lat, lon=lon, start_date='2018-02-01',end_date='2018-02-02')
 
-# # To get an hourly time series of temperature, precipitation, and rh:
-# print forecast.get_series(['precip','temp','rh'])
 
 # ...
-# ```
-
-# The ``get_forecast()`` method requires named parameters. The current choices are either (lat=..., lon=...), (city="City,ST"), or (city=..., state=..., country=...)
