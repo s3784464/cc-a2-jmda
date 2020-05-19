8a06bf440e966ec9da628fff306811db9e379443
Danial_Isman_s3786400 <s3786400@student.rmit.edu.au>
2020-05-19 21:44:22 +1000

    Geocode API implemented

--- code/BasicWeatherApp/weather.py
+++ code/BasicWeatherApp/weather.py
@@ -4,15 +4,21 @@ import pytz
 import requests
 import math
 API_KEY = '9b18e2941607b2d54150439c9ad80bfe'
+GEO_KEY = 'JFeFmdyX5dFybKA3rMVnxzPEk9vZ8_ZzKavdsJHcSbs'
 #API_URL = ('http://api.openweathermap.org/data/2.5/weather?q=Melbourne&mode=json&units=metric&appid=9b18e2941607b2d54150439c9ad80bfe')
 #API_URL = ('https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=minutely,hourly&appid=9b18e2941607b2d54150439c9ad80bfe')
-API_URL = ('https://api.openweathermap.org/data/2.5/onecall?{}&exclude=minutely,hourly&appid={}')
+#API_URL = ('https://api.openweathermap.org/data/2.5/onecall?{}&exclude=minutely,hourly&appid={}')
+API_URL = ('https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=minutely,hourly&appid={}')
+
+GEO_URL = ('https://geocode.search.hereapi.com/v1/geocode?apikey={}&q={}')
 
 def query_api(city):
     try:
         #print(API_URL.format(city, API_KEY))
-        data = requests.get(API_URL.format(city, API_KEY)).json()
-        #data = requests.get(API_URL).json()
+        coord = requests.get(GEO_URL.format(GEO_KEY, city)).json()
+        lat = coord['items'][0]['position']['lat']
+        lon = coord['items'][0]['position']['lng']
+        data = requests.get(API_URL.format(lat, lon, API_KEY)).json()
     except Exception as exc:
         print(exc)
         data = None
